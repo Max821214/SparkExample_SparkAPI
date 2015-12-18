0101a35bf117a5e0cad222c278c0565453516eee
MaxJiang <cathy90703@gmail.com>
2015-12-18 14:42:00 +0800

    This is Spark API example, Version 0.0.1

---
+++ README.md
@@ -0,0 +1,154 @@
+﻿## SparkExample SparkAPI IMAC - BigData Team - 2015/12/16
+
+###問題描述
+利用範例資料測試並使用幾項Spark API
+
+###資料格式
+
+>1.先在HDFS建立```/spark/homework```目錄
+>2.利用```touch test.txt```，建立test.txt，再用```vim test.txt```將資料輸入，如下:
+
+```
+a,123,456,789,11344,2142,123  
+b,1234,124,1234,123,123  
+c,123,4123,5435,1231,5345
+d,123,456,789,113,2142,143  
+e,123,446,789,14,2142,113  
+f,123,446,789,14,2142,1113,323
+```
+
+>2.在利用```hadoop fs -put test.txt /spark/homework```將資料上傳至HDFS
+
+###問題分析
+
+>1.使用map找出測試資料所有英文字母，並存至/spark/homework/map，結果如下:
+
+```
+a
+b
+c
+d
+e
+f
+```
+
+***
+>2.使用flatmap找出測試資料所有以”,”切割的資料，並存至/spark/homework/flatMap,結果如下:
+
+```
+a
+123
+456
+789
+11344
+2142
+123
+b
+1234
+…
+```
+
+***
+>3.使用filter找出測試資料所有以123與456的資料，並存至/spark/homework/filter，結果如下:
+
+```
+123
+456
+123
+1234
+1234
+…
+```
+
+***
+
+>4.使用mapToPair將測試資料轉換成(str, 1)，並存至/spark/homework/mapPair，結果如下:
+
+```
+(a,1)
+(123,1)
+(456,1)
+(789,1)
+(11344,1)
+(2142,1)
+(123,1)
+…
+```
+
+***
+>5.使用flatMapToPair將測試資料轉換成(字母, 所有後面數字的sum)，並存至/spark/homework/filter_output，結果如下：
+
+```
+(a,14977)
+(b,2838)
+(c,16257)
+(d,3766)
+(e,3627)
+(f,4950)
+…
+```
+
+***
+>6.使用groupBy找出測試資料中大於500的資料，若無法辨識分到”None”，並存至/spark/homework/groupBy，結果如下：
+
+```
+(None,[a, b, c, d, e, f])
+(小於 500,[123, 456, 123, 124, 123, 123, 123, 123,
+456, 113, 143, 123, 446, 14, 113, 123, 446, 14, 323])
+(大於 500,[789, 11344, 2142, 1234, 1234, 4123, 5435,
+1231, 5345, 789, 2142, 789, 2142, 789, 2142, 1113])
+…
+```
+
+***
+>7.使用reduce找出測試資料所有英文字母，並用reduce將之append成一個字串，並存至/spark/homework/reduce，結果如下：
+
+```
+abcdef
+```
+
+**
+當中因為reduce輸出為String，需要先加這筆字串加到類別為ArrayList裡，在利用JavaSparkContext中有項parallelize方法將結果轉成JavaRDD輸出
+**
+```java
+	List<String> list = new ArrayList();
+	list.add(reduce);
+	JavaRDD<String> reduceResult = sc.parallelize(list, 1);
+```
+***
+>8.使用reduceByKey找出以”,”切割的所有wordcount，並存至/spark/homework/reduceByKey，結果如下：
+
+```
+(d,1)
+(1113,1)
+(1231,1)
+(e,1)
+(14,2)
+(113,2)
+…
+```
+
+###執行分析
+
+```
+spark-submit --class com.imac.test.Main.class \
+--master yarn-cluster HW.jar \
+/spark/homework/test.txt \
+```
+> 第一行```--class```後面接```Java```的```package name```和```class name```  
+> 第二行--master 為使用叢集模式，這邊採用yarn-cluster，後面接```Jar```  
+> 第三行為```輸入資料```
+
+###輸出結果
+>分析成功後，可以使用```hadoop fs -cat /輸出路徑```指令列出結果，輸出路徑如下:
+
+```
+/spark/homework/map
+/spark/homework/flatMap
+/spark/homework/filter
+/spark/homework/mapPair
+/spark/homework/filter_output
+/spark/homework/groupBy
+/spark/homework/reduce
+/spark/homework/reduceByKey
+```
---
+++ src/com/imac/test/Main.java
@@ -0,0 +1,162 @@
+package com.imac.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+import org.apache.spark.api.java.function.PairFunction;
+
+import scala.Tuple2;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// if (args.length < 2) {
+		// System.exit(1);
+		// }
+
+		SparkConf conf = new SparkConf();
+		conf.setAppName("HWSpark");
+
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaRDD<String> file = sc.textFile(args[0]);
+
+		// TODO:map
+		JavaRDD<String> map = file.map(new Function<String, String>() {
+
+			public String call(String arg0) throws Exception {
+				return arg0.split(",")[0];
+			}
+		});
+
+		map.saveAsTextFile("/spark/homework/map");
+
+		// TODO:flatMap
+		JavaRDD<String> flatmap = file
+				.flatMap(new FlatMapFunction<String, String>() {
+
+					public Iterable<String> call(String arg0) throws Exception {
+						return Arrays.asList(arg0.split(","));
+					}
+				});
+
+		flatmap.saveAsTextFile("/spark/homework/flatMap");
+
+		// TODO:filter
+		JavaRDD<String> filter = flatmap
+				.filter(new Function<String, Boolean>() {
+
+					public Boolean call(String arg0) throws Exception {
+						if (arg0.contains("123") || arg0.contains("456")) {
+							return true;
+						}
+						return false;
+					}
+				});
+
+		filter.saveAsTextFile("/spark/homework/filter");
+
+		// TODO:mapToPair
+		JavaPairRDD<String, Integer> mapToPair = flatmap
+				.mapToPair(new PairFunction<String, String, Integer>() {
+
+					public Tuple2<String, Integer> call(String arg0)
+							throws Exception {
+						return new Tuple2<String, Integer>(arg0, 1);
+					}
+				});
+
+		mapToPair.saveAsTextFile("/spark/homework/mapPair");
+
+		// TODO:flatMapToPair
+		JavaPairRDD<String, Integer> flatMapToPair = file
+				.flatMapToPair(new PairFlatMapFunction<String, String, Integer>() {
+
+					public Iterable<Tuple2<String, Integer>> call(String arg0)
+							throws Exception {
+						String[] word = arg0.split(",");
+						int sum = 0;
+
+						ArrayList<Tuple2<String, Integer>> arrayList = new ArrayList<Tuple2<String, Integer>>();
+
+						for (int i = 0; i < word.length - 1; i++) {
+							sum = sum + Integer.valueOf(word[i + 1]);
+						}
+						arrayList
+								.add(new Tuple2<String, Integer>(word[0], sum));
+						return arrayList;
+					}
+				});
+
+		flatMapToPair.saveAsTextFile("/spark/homework/filter_output");
+
+		// TODO:groupBy (for迴圈會遇到特殊邏輯，碰到一次return即會當作結束)
+		JavaPairRDD<String, Iterable<String>> groupBy = flatmap
+				.groupBy(new Function<String, String>() {
+
+					public String call(String arg0) throws Exception {
+						if (!checkType(arg0)) {
+							return "None";
+						} else {
+							return (Integer.parseInt(arg0) < 500) ? "小於500"
+									: "大於500";
+						}
+					}
+				});
+
+		groupBy.saveAsTextFile("/spark/homework/groupBy");
+
+		// TODO:reduce
+		String reduce = map.reduce(new Function2<String, String, String>() {
+
+			public String call(String arg0, String arg1) throws Exception {
+				// TODO Auto-generated method stub
+				return arg0 + arg1;
+			}
+		});
+
+		List<String> list = new ArrayList();
+		list.add(reduce);
+		JavaRDD<String> reduceResult = sc.parallelize(list, 1);
+		reduceResult.saveAsTextFile("/spark/homework/reduce");
+
+		// TODO:reduceByKey
+		JavaPairRDD<String, Integer> reduceByKey = mapToPair
+				.reduceByKey(new Function2<Integer, Integer, Integer>() {
+
+					public Integer call(Integer arg0, Integer arg1)
+							throws Exception {
+						return arg0 + arg1;
+					}
+				});
+
+		reduceByKey.saveAsTextFile("/spark/homework/reduceByKey");
+	}
+
+	private static boolean checkType(String arg0) {
+		// if(java.lang.Character.isDigit(arg0.charAt(0))){
+		// return true;
+		// }else{
+		// return false;
+		// }
+		try {
+			Integer.parseInt(arg0);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+}
